efeb69504ec7341c0bf7b905b8c5afe7bc1fe122
Brandon Wu <brandonwu23@gmail.com>
2025-10-24 11:57:46 -0700

    Fix build.rs

--- gstscream/build.rs
+++ gstscream/build.rs
@@ -9,11 +9,15 @@ fn main() {
     println!("cargo:warning=FUCK {}", crate_dir.display());
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let lib_dir = out_dir.join("lib");
+    // out_dir.join("../lib").canonicalize().unwrap();
     
 
     let cmakedir = Config::new("..")
         .define("CMAKE_POLICY_VERSION_MINIMUM", "3.5")
         .define("BUILD_SHARED_LIBS", "OFF")
+        // .define("CMAKE_INSTALL_PREFIX", &out_dir)
+        // .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", &out_dir.canonicalize().unwrap())
+        .define("CMAKE_ARCHIVE_OUTPUT_DIRECTORY", lib_dir.to_str().unwrap())
         .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", lib_dir.to_str().unwrap())
         .build_target("scream")
         .profile("Release")
