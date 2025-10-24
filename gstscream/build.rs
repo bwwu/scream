30f637b59f3b5a2cb37d46fc4bd71a6bc8b82fa1
Brandon Wu <brandonwu23@gmail.com>
2025-10-24 11:52:22 -0700

    Output libscream.a to OUT_DIR

--- gstscream/build.rs
+++ gstscream/build.rs
@@ -1,17 +1,24 @@
 extern crate cmake;
+use std::process::Output;
+
 use cmake::Config;
 
 fn main() {
     gst_plugin_version_helper::info();
     let crate_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    let lib_dir = crate_dir.join("../lib").canonicalize().unwrap();
+    println!("cargo:warning=FUCK {}", crate_dir.display());
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let lib_dir = out_dir.join("lib");
+    
 
-    Config::new("..")
+    let cmakedir = Config::new("..")
         .define("CMAKE_POLICY_VERSION_MINIMUM", "3.5")
         .define("BUILD_SHARED_LIBS", "OFF")
+        .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", lib_dir.to_str().unwrap())
         .build_target("scream")
         .profile("Release")
         .build();
+    println!("cargo:warning=cmakedir={}", cmakedir.display());
 
 
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
