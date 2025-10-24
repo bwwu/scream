6d80e0a918ada49f60dcfa551526348f182c08d7
Brandon Wu <brandonwu23@gmail.com>
2025-10-24 10:41:46 -0700

    Statically link libscream into gstscream

--- gstscream/build.rs
+++ gstscream/build.rs
@@ -1,6 +1,18 @@
 
 fn main() {
     gst_plugin_version_helper::info();
+    let crate_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let lib_dir = crate_dir.join("../lib").canonicalize().unwrap();
 
-    println!("cargo:rustc-link-lib=dylib=scream");
+    Config::new("..")
+        .define("CMAKE_POLICY_VERSION_MINIMUM", "3.5")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .build_target("scream")
+        .profile("Release")
+        .build();
+
+
+    println!("cargo:rustc-link-search=native={}", lib_dir.display());
+    println!("cargo:rustc-link-lib=static=scream");
+    println!("cargo:rustc-link-lib=c++");
 }
