75b02a5d17c010758736136ea693b84b665105cf
Brandon Wu <brandonwu23@gmail.com>
2025-11-03 16:29:32 -0800

    Link stdc++

--- gstscream/build.rs
+++ gstscream/build.rs
@@ -1,16 +1,12 @@
 extern crate cmake;
-use std::process::Output;
 
 use cmake::Config;
 
 fn main() {
     gst_plugin_version_helper::info();
-    let crate_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    println!("cargo:warning=FUCK {}", crate_dir.display());
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let lib_dir = out_dir.join("lib");
     // out_dir.join("../lib").canonicalize().unwrap();
-    
 
     let cmakedir = Config::new("..")
         .define("CMAKE_POLICY_VERSION_MINIMUM", "3.5")
@@ -24,8 +20,8 @@ fn main() {
         .build();
     println!("cargo:warning=cmakedir={}", cmakedir.display());
 
-
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
     println!("cargo:rustc-link-lib=static=scream");
+    println!("cargo:rustc-link-lib=stdc++");
     println!("cargo:rustc-link-lib=c++");
 }
